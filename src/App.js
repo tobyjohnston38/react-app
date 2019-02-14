09d1eb364e114731babc116cd7b269e48a6cb587
Toby Johnston <toby@crssoftware.co.nz>
2019-02-14 17:05:18 +1300

    progress so far

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>This is a React app!</h1>
+        <p>thing</p>
+        <button>Switch Name</button>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29" >My Hobbies</Person>
+        <Person name="Stephanie" age="26" />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'this should work'))
