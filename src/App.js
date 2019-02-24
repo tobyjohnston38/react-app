8678a261169492411de356a227486fede6d21bef
Toby Johnston <toby@crssoftware.co.nz>
2019-02-25 11:02:35 +1300

    added keys to person state

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from "./Person/Person"
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 }
+      { id: "dfgf", name: "Max", age: 28 },
+      { id: "fggr", name: "Manu", age: 29 },
+      { id: "ggie", name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
     showPersons: false
@@ -55,6 +55,7 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
+                key={person.id}
               />
             )
           })}
