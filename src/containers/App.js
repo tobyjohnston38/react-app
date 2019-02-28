6a0268f08c1c80b919afe869f65aa4fd9cffd069
Toby Johnston <toby@crssoftware.co.nz>
2019-03-01 10:21:21 +1300

    lifecycle creation

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons"
 import Cockpit from "../components/Cockpit/Cockpit"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("[App.js] constructor")
+  }
+
   state = {
     persons: [
       { id: "dfgf", name: "Max", age: 28 },
@@ -12,6 +17,19 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props)
+    return state
+  }
+
+  componentWillMount() {
+    console.log("[App.js] componentWillMount")
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount")
   }
 
   nameChangedHandler = (event, id) => {
@@ -46,6 +64,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(["App.js render"])
     let persons = null
 
     if (this.state.showPersons) {
