25ad825fcffdcf64c51ab56d47e7d15270c6a41a
Toby Johnston <tobyjohnston38@gmail.com>
2019-03-03 16:40:22 +1300

    added use effect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import classes from "./Cockpit.css"
 
-const cockpit = props => {
+const cockpit = ( props ) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request..
+  })
+
   const assignedClasses = []
   let btnClass = ""
   if (props.showPersons) {
