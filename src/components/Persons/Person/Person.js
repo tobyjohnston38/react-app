b237d101f288cb6e83228933af124f2f6fd300ec
Toby Johnston <tobyjohnston38@gmail.com>
2019-03-10 23:19:30 +1300

    failing adjacent JSX

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
+import Aux from "../../../hoc/Aux"
 import classes from "./Person.css"
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...")
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+    <Aux>
+    <p onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years old
         </p>
         <p>{this.props.children}</p>
@@ -15,7 +16,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+    </Aux>
     )
   }
 }
