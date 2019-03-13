8eccbc953d3f580818ccead7c91200f53de53fcf
Toby Johnston <toby@crssoftware.co.nz>
2019-03-13 16:26:50 +1300

    fixed hoc issue

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const withClass = (WrappedComponent, className) => {
-  return props(
+  return props => (
     <div className={className}>
       <WrappedComponent />
     </div>
