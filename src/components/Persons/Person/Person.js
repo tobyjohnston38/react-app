b7a16c4067fa86788ddfc3d32332125c22dee548
Toby Johnston <toby@crssoftware.co.nz>
2019-03-13 16:57:42 +1300

    unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import Aux from "../../../hoc/Aux"
-import withClasses from "../../../hoc/withClass"
+// import withClasses from "../../../hoc/withClass"
 import withClass from "../../../hoc/withClass"
 import classes from "./Person.css"
 class Person extends Component {
