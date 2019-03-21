510bd323677e02de34f790d28837edfff3029bc4
Toby Johnston <tobyjohnston38@gmail.com>
2019-03-21 22:20:23 +1300

    better usage of context

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import Aux from "../../../hoc/Aux";
-// import withClasses from "../../../hoc/withClass"
 import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
 import AuthContext from "../../../context/auth-context";
@@ -12,19 +11,22 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus()
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
   render() {
     console.log("[Person.js] rendering...");
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p key="i1" onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years old
         </p>
